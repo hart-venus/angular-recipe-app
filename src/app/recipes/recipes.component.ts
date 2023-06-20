8c0964e701da5df1b634187177b17769fb79b015
Ariel Leyva <aleyva1509@gmail.com>
2023-06-20 01:09:05 -0700

    initial recipe type definition

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+type Recipe = {
+  name: string,
+  description: string,
+  imagePath: string
+};
+
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css']
 })
-export class RecipesComponent {
 
+export class RecipesComponent {
+  // recipe type definition
+  recipes: Recipe[] = [
+    {
+      name: 'A Test Recipe',
+      description: 'This is simply a test',
+      imagePath: 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+    },
+    {
+      name: 'Another Test Recipe',
+      description: 'This is simply a test',
+      imagePath: 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+    }
+  ];
+  
 }
